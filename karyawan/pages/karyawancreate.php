7dbe9e566e4b17b1aa7218605d905840a618a1eb
Agung Yoga Setiawan <agungyoga507@gmail.com>
2022-05-12 07:56:25 +0800

    redirect ke karyawan

--- karyawan/pages/karyawancreate.php
+++ karyawan/pages/karyawancreate.php
@@ -24,6 +24,7 @@
                 $statement->bindParam(3, $jenis_kelamin);
                 $statement->bindParam(4, $status_menikah);
                 $statement->execute();
+                header('Location: main.php?page=karyawan');
             }
 
             ?>
